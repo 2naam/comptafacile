4dfd892bafc5994cf04042e7efed9c8af9e8a4af
2naam <nadia.ahmed.univ@gmail.com>
2018-10-25 14:16:04 +0200

    ajout des actions de formulaire

---
+++ actions/connexion.php
@@ -0,0 +1,7 @@
+<p>Bienvenue dans la connexion !</p>
+
+
+<div>Données envoyées :</div> 
+
+<div>Pseudo : <?php echo $_POST['pseudo']; ?> </div>
+<div>Mot de passe : <?php echo $_POST['password']; ?> </div>
---
+++ actions/inscription.php
@@ -0,0 +1,10 @@
+<p>Bienvenue dans l'inscription</p>
+
+
+<div>Données envoyées :</div> 
+
+
+<div>Email : <?php echo $_POST['email']; ?> </div>
+<div>Pseudo : <?php echo $_POST['pseudo']; ?> </div>
+<div>Mot de passe : <?php echo $_POST['password']; ?> </div>
+<div>Confirmation : <?php echo $_POST['password2']; ?> </div>
