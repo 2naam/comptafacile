18d18e58bd8ec6f608e055958cf70ebefb20e586
Rizak <rizak.ahmed@gmail.com>
2018-10-25 17:13:29 +0200

    verification du pseudo dans le fichier utilisateurs

--- actions/connexion.php
+++ actions/connexion.php
@@ -1,29 +1,46 @@
 <p>Bienvenue dans la connexion !</p>
 
-
-<div>Données envoyées :</div> 
-
-<div>Pseudo : <?php echo $_POST['pseudo']; ?> </div>
-<div>Mot de passe : <?php echo $_POST['password']; ?> </div>
 <?php
+//récupération des champs de formulaire
+$pseudo = null;
+$password = null;
+if(isset($_POST['pseudo']) ){$pseudo= $_POST['pseudo'];}
+if(isset($_POST['password']) ){$password= $_POST['password'];}
 
-// Test de l'attribution du pseudo 
+//Definition des messages d'erreur
+$Erreurs = array();
 
-if (isset($_POST['pseudo']) ) {
-	
-	echo "Pseudo non reconnu";
+//Chemin du fichier utilisateurs
+$filePath = '../data/utilisateurs.txt';
+
+// On ouvre le fichier ou on le crée s'il existe pas
+$userFile = fopen("".$filePath, 'a+');
+
+if ($pseudo != null) {
+	$pseudotrouve = false;
+	// Test de l'attribution du pseudo 
+	while(!feof($userFile)) {
+        //Lecture d'une ligne
+		$line = fgets($userFile);
+        //Separation de la ligne sur le charactere ','
+		$splittedLine = explode(",", $line);
+		//Test de la présence du pseudo en deuxième position de la ligne
+		if ($splittedLine[1] == $pseudo){
+			$pseudotrouve = true;
+		}
+    }
+	if($pseudotrouve){
+		echo "<div>Pseudo reconnu</div>";
+		echo "<div>Vous êtes connecté !</div>";
+	} else {
+		echo "<div>Pseudo non reconnu</div>";
+		echo "<div>Cassez vous d'ici!</div>";
+		$Erreurs['user'] = "Pseudo inconnu ! ";
+	}	
+} else {
+		$Erreurs['user'] = "Utilisateur non défini ! ";
 }
 
-// On ouvre le fichier
-$monfichier = fopen('data/utilisateurs.txt', 'r+');
-
-
-
-
-
 // On ferme le fichier
-
-fclose($monfichier);
-
-echo "Vous êtes connecté !";
+fclose($userFile);
 ?>
