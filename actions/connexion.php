bcd9983133d01446ce3938a7fe87b7f3c74704c5
2naam <nadia.ahmed.univ@gmail.com>
2018-10-25 15:47:48 +0200

    update index.html

--- actions/connexion.php
+++ actions/connexion.php
@@ -5,3 +5,25 @@
 
 <div>Pseudo : <?php echo $_POST['pseudo']; ?> </div>
 <div>Mot de passe : <?php echo $_POST['password']; ?> </div>
+<?php
+
+// Test de l'attribution du pseudo 
+
+if (isset($_POST['pseudo']) ) {
+	
+	echo "Pseudo non reconnu";
+}
+
+// On ouvre le fichier
+$monfichier = fopen('data/utilisateurs.txt', 'r+');
+
+
+
+
+
+// On ferme le fichier
+
+fclose($monfichier);
+
+echo "Vous êtes connecté !";
+?>
