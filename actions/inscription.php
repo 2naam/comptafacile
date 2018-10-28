eab3cea1a465c8a9b6d8984aa6f18d88d6c99e82
2naam <nadia.ahmed.univ@gmail.com>
2018-10-29 00:34:53 +0100

    rebase

--- actions/inscription.php
+++ actions/inscription.php
@@ -77,11 +77,3 @@ if ($user->isEmailValid()) {
 <p>Bienvenue dans l'inscription</p>
 
 
-<div>Données envoyées :</div> 
-
-<!-- Champ d'affichage du formulaire --> 
-
-<div>Email : <?php echo $_POST['email']; ?> </div>		
-<div>Pseudo : <?php echo $_POST['pseudo']; ?> </div>
-<div>Mot de passe : <?php echo $_POST['password']; ?> </div>
-<div>Confirmation : <?php echo $_POST['password2']; ?> </div>
