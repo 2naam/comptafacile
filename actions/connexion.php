b69a5289924ec04370c34f3fd1a77e61c59e2488
2naam <nadia.ahmed.univ@gmail.com>
2018-10-30 23:23:11 +0100

    Messages erreurs et succès connexion et inscription.php

--- actions/connexion.php
+++ actions/connexion.php
@@ -1,36 +1,37 @@
-<p>Bienvenue dans la connexion !</p>
+<h2>Bienvenue dans la connexion !</h2>
 
 <?php
 
 require_once('../model/User.php');
 require_once('../tools/FileReader.php');
+session_start();
 
 //récupération des champs de formulaire
-
 $user = null; 
 $Erreurs = array();
+$Success = array();
 
 if(isset($_POST['pseudo']) &&  isset($_POST['password'])){
 	$user = new User(trim($_POST['pseudo']),trim($_POST['password']));
+	// Verification du pseudo 
+	if ($user->isValidForConnect()){
+		$fileReader = new FileReader('../data/utilisateurs.txt',",");
+		$passwordMatch = $fileReader -> find($user->getPseudo(),1,$user->getPassword(),2);	
+		
+		if($passwordMatch){
+			echo "<div>Vous êtes connectez !</div>";
+			$Success['user'] = "Utilisateur connecté";
+			$_SESSION['user'] = $user;
+		} else {
+			$Erreurs['user'] = "Pseudo ou mot de passe incorrecte !";
+		}	
+	} else {
+			$Erreurs['user'] = "Identifiant(s) manquant(s) ou incorrecte(s) ";
+	}
 } else{
 	$Erreurs['user'] = "Veuillez saisir tous vos identifiants ! ";
-	echo "<div>Veuillez saisir tous vos identifiants !</div>";
 }
-
-// Verification du pseudo 
-if ($user->isValidForConnect()){
-	$fileReader = new FileReader('../data/utilisateurs.txt',",");
-	$passwordMatch = $fileReader -> find($user->getPseudo(),1,$user->getPassword(),2);	
-	
-	if($passwordMatch){
-		echo "<div>Vous êtes connectez !</div>";
-	} else {
-		echo "<div>Pseudo ou mot de passe incorrecte !</div>";
-		echo "<div>Cassez vous d'ici!</div>";
-		$Erreurs['user'] = "Pseudo ou mot de passe incorrecte !";
-	}	
-} else {
-		$Erreurs['user'] = "Identifiant(s) manquant(s) ou incorrecte(s) ";
-		echo "<div>Veuillez saisir tous vos identifiants !</div>";
-}
-?>
+$_SESSION['erreurs'] = $Erreurs;
+$_SESSION['success'] = $Success;
+$_SESSION['pseudo'] = $user -> getPseudo();
+header("Location: http://localhost/comptafacile");
--- actions/inscription.php
+++ actions/inscription.php
@@ -3,9 +3,11 @@
 <?php
 require_once('../model/User.php');
 require_once('../tools/FileReader.php');
+session_start();
 
 $user = null; 
 $Erreurs = array();
+$Success = array();
 
 if(isset($_POST['email']) && isset($_POST['pseudo']) && isset($_POST['password']) && isset($_POST['password2'])){
 		$user = new user(trim($_POST['email']),trim($_POST['pseudo']),trim($_POST['password']));
@@ -16,41 +18,40 @@ if(isset($_POST['email']) && isset($_POST['pseudo']) && isset($_POST['password']
 				$fileReader = new FileReader('../data/utilisateurs.txt',",");
 				// Test de l'attribution de l'email 
 				if($fileReader->find($user->getEmail(),0, null, null)){
-					echo "<div>Email déjà attribuée</div>";
 					$Erreurs['user'] = "Email déjà attribuée ! ";
 				} else {		
 					// Test de l'attribution du pseudo 
 					if($fileReader->find($user->getPseudo(),1, null, null)){
-						echo "<div>Pseudo déjà attribué </div>";
 						$Erreurs['user'] = "Pseudo déjà attribué ! ";
 					} else {
 						$fileReader->write($user->getEmail().",".$user->getPseudo().",".$user->getPassword()."\r\n");
-						echo "<div>Utilisateur enregistré en base! </div>";
+						$Success['user'] = "Félicitation ".$user->getPseudo()." ! Votre compte a été créé.";
+						$_SESSION['user'] = $user;
 					}			
 				}	
 			} else {
-				echo "Le mot de passe ne correspond pas à la confirmation";
 				$password2 = null;
 				$Erreurs['password2'] = "Le mot de passe ne correspond pas à la confirmation"; 
 			}			
 		}
 		else{
 			if(!$user->isValidEmail()){
-				echo "<div> Email invalide </div>";
 				$Erreurs['email'] = "Email invalide !";
 			}
 			if(!$user->isValidPseudo()){
-				echo "<div> Pseudo invalide </div>";
 				$Erreurs['pseudo'] = "Pseudo invalide !";
 			}
 			if(!$user->isValidPassword()){
-				echo "<div>Mot de passe invalide ! </div>";
 				$Erreurs['password'] = "Mot de passe invalide ! ";
 			}
 		}
 }
 else {
 	$Erreurs['user'] = "Veuillez saisir tous vos identifiants ! ";
-	echo "<div>Veuillez saisir tous vos identifiants !</div>";
 }
+$_SESSION['email'] = $user -> getEmail(); 
+$_SESSION['pseudo'] = $user -> getPseudo(); 
+$_SESSION['erreurs'] = $Erreurs;
+$_SESSION['success'] = $Success;
+header("Location: http://localhost/comptafacile");
 ?>
